acd2467d7aabc02cf30de9f27e1b04fa715899b8
Andon-Brooks <brooks79@miamioh.edu>
2024-10-30 15:18:16 +0000

    CHanging room class

--- Room.java
+++ Room.java
@@ -2,19 +2,27 @@
 public abstract class Room {
     private string roomName;
 
-    private boolean wasGary;
+    public boolean wasGary;
 
     Room(String name) {
         this.roomName = name;
     }
 
-    public abstract void trap();
+    public void situation() {
+        Scanner scan = new Scanner(System.in);
 
-    public abstract void puzzle();
+        // if gary.currentRoom = true it is automatically catchGary
+        // if wasGary = true then randomly pick a sistuion to do
+        // otherwise print a message that says that gary isnt there and nothing is wrong
+    }
 
-    public abstract void problem();
+    private abstract void trap(Scanner sc);
 
-    public abstract void taunt();
+    private abstract void puzzle(Scanner sc);
 
-    public abstract void catchGary();
+    private abstract void problem(Scanner sc);
+
+    private abstract void taunt(Scanner sc);
+
+    private abstract void catchGary(Scanner sc);
 }
