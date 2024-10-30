e9693a712ac81b127ae9e3961266845cfeb38b0a
Brynn Willard <willarbh@miamioh.edu>
2024-10-30 10:50:52 -0400

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,20 +1,31 @@
-
 public abstract class Room {
-    private string roomName;
-
+    private String roomName;
     private boolean wasGary;
 
-    Room() {
+    Room(String roomName) {
+        this.roomName = roomName;
+        this.wasGary = false; // Initially, Gary has not been in this room
+    }
 
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public boolean wasGary() {
+        return wasGary;
+    }
+
+    public void setWasGary(boolean wasGary) {
+        this.wasGary = wasGary;
     }
 
     public abstract void trap();
-
+    
     public abstract void puzzle();
-
+    
     public abstract void problem();
-
+    
     public abstract void taunt();
-
+    
     public abstract void catchGary();
 }
